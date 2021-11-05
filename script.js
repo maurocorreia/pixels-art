c15a1c47e509af6e568477bff75b2cbf6f3b4201
Mauro Correia <mauroac1@outlook.com>
2021-11-05 07:41:59 -0300

    Lint.

--- script.js
+++ script.js
@@ -1,63 +1,59 @@
-const pixels = document.querySelectorAll(".pixel");
 const board = document.getElementById('pixel-board');
-const button = document.querySelector("#clear-board");
-const corUm = document.querySelector("#corUm");
-const corDois = document.querySelector("#corDois");
-const corTres = document.querySelector("#corTres");
-const corQuatro = document.querySelector("#corQuatro");
-const colors = document.querySelectorAll(".color");
+const button = document.querySelector('#clear-board');
+const corUm = document.querySelector('#corUm');
+const corDois = document.querySelector('#corDois');
+const corTres = document.querySelector('#corTres');
+const corQuatro = document.querySelector('#corQuatro');
+const colors = document.querySelectorAll('.color');
+const colorSelec = 'color selected';
 
 colors[0].style.backgroundColor = 'black';
 colors[1].style.backgroundColor = 'seagreen';
 colors[2].style.backgroundColor = '#2ec4b6';
 colors[3].style.backgroundColor = '#bee1e6';
 
-
 for (let i = 0; i < 25; i += 1) {
   const pixel = document.createElement('div');
   board.appendChild(pixel);
   pixel.className = 'pixel';
-  pixel.addEventListener('click', function () {
+  pixel.addEventListener('click', () => {
     const colorSelected = document.querySelector('.selected');
-    console.log(colorSelected.style.backgroundColor)
+    console.log(colorSelected.style.backgroundColor);
     pixel.style.backgroundColor = colorSelected.style.backgroundColor;
-  })
+  });
 }
 
-corUm.addEventListener('click', function () {
-  for (i = 0; i < colors.length; i += 1) {
+corUm.addEventListener('click', () => {
+  for (let i = 0; i < colors.length; i += 1) {
     colors[i].className = 'color';
   }
-  corUm.className = 'color selected';
-})
+  corUm.className = colorSelec;
+});
 
-corDois.addEventListener('click', function () {
-  for (i = 0; i < colors.length; i += 1) {
+corDois.addEventListener('click', () => {
+  for (let i = 0; i < colors.length; i += 1) {
     colors[i].className = 'color';
   }
-  corDois.className = 'color selected';
-})
+  corDois.className = colorSelec;
+});
 
-corTres.addEventListener('click', function () {
-  for (i = 0; i < colors.length; i += 1) {
+corTres.addEventListener('click', () => {
+  for (let i = 0; i < colors.length; i += 1) {
     colors[i].className = 'color';
   }
-  corTres.className = 'color selected';
-})
+  corTres.className = colorSelec;
+});
 
-corQuatro.addEventListener('click', function () {
-  for (i = 0; i < colors.length; i += 1) {
+corQuatro.addEventListener('click', () => {
+  for (let i = 0; i < colors.length; i += 1) {
     colors[i].className = 'color';
   }
-  corQuatro.className = 'color selected';
-})
+  corQuatro.className = colorSelec;
+});
 
-
-
-button.addEventListener('click', function (event) {
-  const pixels = document.querySelectorAll(".pixel");  
+button.addEventListener('click', () => {
+  const pixels = document.querySelectorAll('.pixel');
   for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].style.backgroundColor = 'white';
   }
-})
-
+});
